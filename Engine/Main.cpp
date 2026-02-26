c6e1b9ac5eaa8b39934d05ea00cb2b455f454617
ronniej <ronniej@naver.com>
2026-02-26 14:51:39 +0900

    복습 & 코드 정리
    - GraphicsContext 코드 정리

--- Engine/Main.cpp
+++ Engine/Main.cpp
@@ -29,6 +29,7 @@ int main()
 	// 창 생성.
 	Craft::Win32Window window(
 		width, height,
+		title,
 		hInstance,
 		WindowProc
 	);
@@ -41,7 +42,7 @@ int main()
 
 	// 장치 생성 테스트.
 	Craft::GraphicsContext context;
-	context.Initialize(width, height, window);
+	context.Initialize(window);
 
 	// 창에서 발생하는 메시지 처리 루프.
 	// GetMessage - 동기 방식(Blocking 방식).
